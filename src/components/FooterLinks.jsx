50134e65b5370b3ccba424656caa5fe97aca0c0e
vivek <viv2005ek@gmail.com>
2024-08-01 22:47:01 +0530

    netlify production

--- src/components/FooterLinks.jsx
+++ src/components/FooterLinks.jsx
@@ -7,14 +7,18 @@ import FooterAbout from "./FooterAbout";
 
 function FooterLinks() {
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between	gap-x-10 gap-y-10 mt-10 border-b-4 pb-8 items-center w-[100%] min-[350px]:w-[90%] md:w-[80%] ml-[2.5%]  min-[350px]:ml-[5%] md:ml-[10%]">
-      <FooterAbout />
-      <div className="flex justify-between gap-20">
-        <LinkLeftFooter />
-        <AlsoVisitFooter />
+    <div className="flex flex-col border-b-4 pb-8  w-[100%] min-[350px]:w-[90%] md:w-[80%] ml-[2.5%]  min-[350px]:ml-[5%] md:ml-[10%] gap-y-20">
+      <div className="flex flex-col lg:flex-row lg:justify-between	gap-x-10 gap-y-10 mt-10  ">
+        <FooterAbout />
+        <div className="flex justify-between gap-20">
+          <LinkLeftFooter />
+          <AlsoVisitFooter />
+          {/* <EventsFooterLink /> */}
+        </div>
       </div>
-      {/* <NewsLetter /> */}
-      {/* <EventsFooterLink /> */}
+      <div className="self-end	">
+        <NewsLetter />
+      </div>
     </div>
   );
 }
